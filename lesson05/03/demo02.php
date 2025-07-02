c247ed0a701d7d7a1f38a3b01d836679c7eb3c49
miniGungor <siski_02@internet.ru>
2025-07-03 01:10:37 +0600

    lesson05-example3

---
+++ lesson05/03/demo02.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace lesson05\player\demo02;
+
+class Player
+{
+    public $volume = 5;
+    public $state;
+    public $track;
+
+    public function play()
+    {
+
+    }
+
+    public function stop()
+    {
+
+    }
+
+    public function prev()
+    {
+
+    }
+
+    public function next()
+    {
+
+    }
+
+}
+
+$player = new Player();
+$player->play();
